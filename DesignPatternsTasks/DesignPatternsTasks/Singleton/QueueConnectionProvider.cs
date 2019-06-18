e1e4be57d27c9c5e4ecb9c8d404e2402859a93f3
Yelyzaveta Shalamai <yelyzaveta_shalamai@epam.com>
2019-06-18 07:40:45 +0300

    add tests and implementation for singleton

---
+++ DesignPatternsTasks/DesignPatternsTasks/Singleton/QueueConnectionProvider.cs
@@ -0,0 +1,38 @@
+﻿namespace DesignPatternsTasks.Singleton
+{
+    //public class QueueConnectionProvider
+    //{
+    //    private static readonly QueueConnectionProvider instance = new QueueConnectionProvider();
+
+    //    public static QueueConnectionProvider GetInstance()
+    //    {
+    //        return instance;
+    //    }
+    //}
+
+    public class QueueConnectionProvider
+    {
+        private static QueueConnectionProvider instance;
+
+        private static object locker = new object();
+
+        private QueueConnectionProvider()
+        {
+            instance = new QueueConnectionProvider();
+        }
+
+        public static QueueConnectionProvider GetInstance()
+        {
+            if (instance == null)
+            {
+                lock (locker)
+                {
+                    if (instance == null)
+                        instance = new QueueConnectionProvider();
+                }
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ DesignPatternsTasks/DesignPatternsTests/SingletonTests.cs
@@ -0,0 +1,32 @@
+﻿using DesignPatternsTasks.Singleton;
+using FluentAssertions;
+using NUnit.Framework;
+using System.Threading;
+
+namespace DesignPatternsTests
+{
+    [TestFixture]
+    public class SingletonTests
+    {
+        [Test]
+        public void ShouldReturnSameObject_ForSingleThread()
+        {
+            var queueConnectionProvider1 = QueueConnectionProvider.GetInstance();
+            var queueConnectionProvider2 = QueueConnectionProvider.GetInstance();
+            queueConnectionProvider1.Should().BeSameAs(queueConnectionProvider2);
+        }
+
+        [Test]
+        public void ShouldReturnSameObject_ForSeveralThreads()
+        {
+            QueueConnectionProvider queueConnectionProvider1 = null;
+            new Thread(() =>
+            {
+                queueConnectionProvider1 = QueueConnectionProvider.GetInstance();
+            }).Start();
+          
+            var queueConnectionProvider2 = QueueConnectionProvider.GetInstance();
+            queueConnectionProvider1.Should().BeSameAs(queueConnectionProvider2);
+        }
+    }
+}
